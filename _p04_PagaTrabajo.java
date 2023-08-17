15506f6a0b52df1eeda5cdeee7080259e1036fb8
EHL1233 <eduar.ere@gmail.com>
2023-08-17 09:15:12 -0600

    v4

---
+++ _p04_PagaTrabajo.java
@@ -0,0 +1,28 @@
+// Calcula la paga de un trabajador
+import java.util.Scanner;
+
+public class _p04_PagaTrabajo {
+    public static void main(String[] args) {
+        // declarar variables
+        String nombre;
+        int horas;
+        float paga, pagabruta, tasa, impuesto, paganeta;
+        Scanner entrada = new Scanner(System.in);
+       // entrada de datos
+       System.out.println("Nombre           :"); nombre = entrada.nextLine();
+       System.out.println("Horas de trabajo :"); horas = entrada.nextInt();
+       System.out.println("Paga por hora    :"); paga = entrada.nextFloat();
+       tasa = 0.3f;
+       // calculo 
+       pagabruta = horas*paga;
+       impuesto = pagabruta*tasa;
+       paganeta = pagabruta-impuesto;
+       //salida
+       System.out.println("Resumen de pagos \n");
+       System.out.println(String.format("El trabajador %s, trabajo %d horas, con una paga de %.2f pesos la hora, se asume una tasa de impuesto de %.2f ", nombre, horas, paga, tasa));
+       System.out.println(String.format("Paga bruta: %.2f", pagabruta));
+       System.out.println(String.format("Impuesto: %.2f", impuesto));
+       System.out.println(String.format("Paga neta: %.2f", paganeta));
+
+    }
+}
