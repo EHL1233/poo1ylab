350422616c8f3a8404f4c0b3178e2089b0782318
EHL1233 <eduar.ere@gmail.com>
2023-08-17 08:28:45 -0600

    v2

--- _p01_HolaMundo.java
+++ _p01_HolaMundo.java
@@ -2,6 +2,13 @@
 
 public class _p01_HolaMundo{
     public static void main(String[] args) {
+        String amigo = "Shinji";
+        String hermano = "Gendo";
+        String mensaje = String.format("Tanto %s como %s desean aprender a programar en Java", amigo, hermano);
+
         System.out.println("Hola Mundo en el lenguaje Java");
+        System.out.println("\nHola amigo "+amigo+" bienvenido a Java");
+        System.out.println("\nMi amigo es "+amigo+" y mi hermano es "+hermano+"\n");
+        System.out.println(mensaje);
     }
 }
